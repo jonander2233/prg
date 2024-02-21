4b6f61701bcd731ff9a11ea7c399948c56d277df
Jon Ander <jonander2233@gmail.com>
2024-02-22 00:10:22 +0100

    21/2/24 3:01

--- src/main/java/org/jonander2233/tareas/tema07pilascolas/Ej05.java
+++ src/main/java/org/jonander2233/tareas/tema07pilascolas/Ej05.java
@@ -1,5 +1,31 @@
 package org.jonander2233.tareas.tema07pilascolas;
 
-public class Ej05 {
+import org.jonander2233.lib_personal.Pila;
 
+import java.util.Scanner;
+
+public class Ej05 {
+    public void ej05(){
+        StringBuilder sb = new StringBuilder();
+        Scanner lector = new Scanner(System.in);
+        String[] datos = new String[10];
+        int tamanyo;
+        String texto;
+        System.out.println("introduce tu operacion");
+        texto = lector.nextLine();
+
+        char caracter;
+        for (int i = 0; i < texto.length(); i++) {
+            caracter = texto.charAt(i);
+            if (Character.isDigit(caracter)){
+                sb.append(caracter);
+            } else if (comparadorOperadores(caracter)) {
+
+            }
+        }
+        Pila pila = new Pila(tamanyo);
+    }
+    private static boolean comparadorOperadores(char car){
+        return car == '+' && car == '-' && car == '*' && car == '/' && car == '%';
+    }
 }
