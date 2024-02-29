76a9d9d1dadef82cea7f6774c1245913a1cfcf54
Jon Ander <jonander2233@gmail.com>
2024-02-29 14:56:33 +0100

    wip

--- src/main/java/org/jonander2233/tareas/tareas_t08/EjercicioCalculadora.java
+++ src/main/java/org/jonander2233/tareas/tareas_t08/EjercicioCalculadora.java
@@ -13,6 +13,12 @@ public class EjercicioCalculadora {
         JPanel panel = new JPanel();
 
         //layouts
+        panel.setLayout(new GridLayout(2,1));
+        JButton boton = new JButton("boton1");
+        JButton boton2 = new JButton("boton2");
+//        JLabel pantallaCalc = new JLabel("pantalla");
+        ventana.add (boton,boton2);
+
 
 
         //inicio las variables de tamaño y posición de la ventana
@@ -23,14 +29,37 @@ public class EjercicioCalculadora {
 
         //establezco la posición X Y en el centro de la pantalla
 
-//        posX = (int)pantalla.getHeight() / 2 - ;
+        posX = (int)pantalla.getWidth() / 2 -(ancho/2) ;
+        posY = (int)pantalla.getHeight() / 2 -(alto/2) ;
 
         //dimensiono la ventana
         ventana.setSize(ancho,alto);
 
         //posiciono la ventana
-//        ventana.setBounds();
+        ventana.setBounds(posX,posY,ancho,alto);
+
+        //la ventana ya no se redimensiona
+        ventana.setResizable(false);
+
         //hago la ventana visible
         ventana.setVisible(true);
+
+        //se termina el proceso cuando se cierra la ventana
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+
+
+//        tk.createImage("https://raja.scene7.com/is/image/Raja/products/calculadora-sobremesa-casio_PDT17590.jpg?template=withpicto410&$image=M_MS80VER_S_ES&$picto=NoPicto&hei=410&wid=410&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0");
+//        ventana.pack();-------------Redimensiona la ventana para ajustarse al tamaño de los layouts y
+//componentes que contiene
+
+
+
     }
 }
