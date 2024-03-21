553584b42d2c0bb4c4db73701245bceda8bac775
Jon <jonander2233@gmail.com>
2024-03-21 20:47:57 +0100

    wip

--- src/main/java/org/jonander2233/tareas/tema08/ej08/Ej08.java
+++ src/main/java/org/jonander2233/tareas/tema08/ej08/Ej08.java
@@ -22,32 +22,32 @@ public class Ej08 {
         frame.setContentPane(panel);
         //cuadro de texto 1
         JTextField texto1Cuadro = new JTextField(20);
-        texto1Cuadro.setBounds(100,50,500,30);
+        texto1Cuadro.setBounds(150,50,500,30);
 
         //cuadro de texto 2
         JTextField texto2Cuadro = new JTextField(20);
-        texto2Cuadro.setBounds(100,100,500,30);
+        texto2Cuadro.setBounds(150,100,500,30);
 
         //cuadro de texto semilla
         JTextField semillaCuadro = new JTextField(20);
-        semillaCuadro.setBounds(100,200,100,30);
+        semillaCuadro.setBounds(150,200,100,30);
 
         //texto1
         JLabel texto1 = new JLabel("TEXTO A ENCRIPTAR");
         texto1.setForeground(new Color(0,0,0,100));
-        texto1.setBounds(110,50,500,30);
+        texto1.setBounds(10,50,500,30);
 
 
         //texto2
         JLabel texto2 = new JLabel("TEXTO ENCRIPTADO");
         texto2.setForeground(new Color(0,0,0,100));
-        texto2.setBounds(110,100,500,30);
+        texto2.setBounds(10,100,500,30);
 
 
         //texto de la semilla
         JLabel textoSemilla = new JLabel("SEMILLA");
         textoSemilla.setForeground(new Color(0,0,0,100));
-        textoSemilla.setBounds(110,200,500,30);
+        textoSemilla.setBounds(80,200,500,30);
 
 
 
--- src/main/java/org/jonander2233/tareas/tema08/ej08/Logic.java
+++ src/main/java/org/jonander2233/tareas/tema08/ej08/Logic.java
@@ -6,32 +6,55 @@ import java.util.Random;
 public class Logic {
     private int seed;
     Random random = new Random(seed);
-
+    private String letras = "AÁÀaáàBbCcDdEÉÈeéèFfGgHhIÍÌiíìJjKkLlMmNnÑnOÓÒoóòPpQqRrSsTtUÚÙuúùVvWwXxYÝyýZz";
     public Logic(int seed) {
         this.seed = seed;
     }
+
 
     public Logic() {
         this.seed = random.nextInt(100000,999999);
     }
 
+//    public String encrypt1(String text,int key){
+//        int offset = (pos + key)  % letras.length();
+//    }
+
     public String encrypt(String text){
         StringBuilder sb = new StringBuilder();
-        int place;
-        int character;
-        int intResult;
-        int charResult;
-        String encrypted ="";
+        int posLetra;
+        int posFinal;
+        char letra;
         for (int i = 0; i < text.length(); i++) {
-            place = random.nextInt(1,27);
-            character = (int) text.charAt(i);
-            intResult = character + place;
-            charResult = (char)intResult;
-            sb.append(charResult);
-            encrypted = sb.toString();
+            letra = text.charAt(i);
+            posLetra = letras.indexOf(letra);
+            posFinal = posLetra + random.nextInt(1,letras.length());
+            if (posFinal > letras.length()){
+                posFinal = posFinal - letras.length();
+
+            }
+            sb.append(letras.charAt(posFinal));
         }
+        /*
+
+        int result;
+        char letraTexto;
+        for (int i = 0; i < text.length(); i++) {
+            letraTexto = text.charAt(i);
+            for (int j = 0; j < letras.length(); j++) {
+                if (letraTexto == letras.charAt(j)){
+                   result = j + random.nextInt(letras.length());
+                   if ( result > letras.length()){
+                       result = result - letras.length();
+                   }
+                }
+            }
+        }
+
+         */
+
         reset();
-        return encrypted;
+        return "s";
     }
 
     public String decrypt(String text) {
