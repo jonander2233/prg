3a367c1020f5c98ae93cd09782229dbbcadff99d
Jon <jonander2233@gmail.com>
2024-04-12 16:55:01 +0200

    wip

--- src/main/java/org/jonander2233/Main.java
+++ src/main/java/org/jonander2233/Main.java
@@ -1,11 +1,11 @@
 package org.jonander2233;
-import org.jonander2233.tareas.tema09.ej02.Ej02;
+import org.jonander2233.tareas.tema09.ej05.Ej05;
 
 import javax.swing.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        Ej02.ex();
+        Ej05.ex();
     }
 } 
--- src/main/java/org/jonander2233/tareas/tema09/ej05/Ej05.java
+++ src/main/java/org/jonander2233/tareas/tema09/ej05/Ej05.java
@@ -4,6 +4,8 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ej05 {
+    private static Scanner lector = new Scanner(System.in);
+
     public static void ex(){
         Alumno al1 = new Alumno();
         Alumno al2 = new Alumno();
@@ -20,44 +22,44 @@ public class Ej05 {
     private static void preguntarAl (Alumno al1,Alumno al2){
         boolean valido = false;
         System.out.println("indica el nombre primer alumno");
+        al1.setNombre(lector.nextLine());
         preguntarEdad(al1);
         preguntarAltura(al1);
 
         System.out.println("indica el nombre segundo alumno");
-        preguntarAltura(al2);
+        al2.setNombre(lector.nextLine());
         preguntarEdad(al2);
+        preguntarAltura(al2);
 
     }
     private static void preguntarEdad(Alumno al){
-        Scanner lector = new Scanner(System.in);
+
         boolean valido = false;
+        int edad;
         System.out.println("ingrese la edad del alumno");
         do{
             try {
-                al.setEdad(lector.nextInt());
+                edad = Integer.parseInt(lector.nextLine());
+                al.setEdad(edad);
                 valido = true;
-            }catch (InputMismatchException ime){
+            }catch (NumberFormatException nfe){
                 System.out.println("caracter invalido");
             }
         }while (!valido);
 
-        lector.close();
     }
     private static void preguntarAltura(Alumno al){
-        Scanner lector = new Scanner(System.in);
         boolean valido = false;
         float altura;
         System.out.println("ingrese la altura del alumno");
         do{
             try {
-                al.setAltura(lector.nextFloat());
+                al.setAltura(Float.parseFloat(lector.nextLine()));
                 valido = true;
-            }catch (InputMismatchException ime){
+            }catch (NumberFormatException nfe){
                 System.out.println("caracter invalido");
 
             }
         }while (!valido);
-
-        lector.close();
     }
 }
