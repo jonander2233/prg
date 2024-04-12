3cecabd282b7344eeedc6013338e24a58fcccb19
Jon <jonander2233@gmail.com>
2024-04-12 17:05:11 +0200

    wip

---
+++ src/main/java/org/jonander2233/tareas/tema09/ej06/Ej06.java
@@ -0,0 +1,10 @@
+package org.jonander2233.tareas.tema09.ej06;
+
+public class Ej06 {
+    public static void dividirEntreArray (int num, int[] nums){
+        int[] resultado = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+//            nums[i]
+        }
+    }
+}
