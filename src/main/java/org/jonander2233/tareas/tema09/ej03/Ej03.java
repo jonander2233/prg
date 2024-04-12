c42d664723ab6cd66d16a63af0a59397997ddb9f
Jon <jonander2233@gmail.com>
2024-04-12 15:29:34 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema09/ej03/Ej03.java
+++ src/main/java/org/jonander2233/tareas/tema09/ej03/Ej03.java
@@ -3,14 +3,21 @@ package org.jonander2233.tareas.tema09.ej03;
 import org.jonander2233.lib_personal.GenericDynamicArray;
 
 public class Ej03 {
-    public static void ex(){
+    public static void ex() {
         GenericDynamicArray<Integer> array = new GenericDynamicArray<>();
-        boolean valid=false;
-        int numMayor =0;
+        boolean valid = false;
+        boolean salir = false;
+        int numMayor = 0;
+        do {
+
+        } while (!salir);
+
 
         for (int i = 1; i < array.size(); i++) {
-            if(array.get(i)>numMayor){
+            if (array.get(i) > numMayor) {
                 numMayor = array.get(i);
             }
+
+        }
     }
 }
