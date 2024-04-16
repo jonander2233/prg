d060df1824cfc33ec9dd260fe1d06e93cebaa1c3
Jon <jonander2233@gmail.com>
2024-04-16 15:40:19 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema09/ej07/Ej07.java
+++ src/main/java/org/jonander2233/tareas/tema09/ej07/Ej07.java
@@ -1,10 +1,10 @@
 package org.jonander2233.tareas.tema09.ej07;
 public class Ej07 {
     Ej07(){
-        String[] array = {"Hola", "Adios", null, "Buenos d�as", "Buenas tardes", "Buenas noches"};
-        String[] array2 = {"Hola", "Adios", "Buenos d�as", "Buenas tardes", "Buenas noches"};
-        String[] array3 = {"Hola", "Adios", null, "Buenos d�as", "Buenas tardes", "Buenas noches", null};
-        String[] array4 = {"Hola", "Adios", null, "Buenos d�as", "Buenas tardes", "Buenas noches", null, null};
+        String[] array = {"Hola", "Adios", null, "Buenos dias", "Buenas tardes", "Buenas noches"};
+        String[] array2 = {"Hola", "Adios", "Buenos dias", "Buenas tardes", "Buenas noches"};
+        String[] array3 = {"Hola", "Adios", null, "Buenos dias", "Buenas tardes", "Buenas noches", null};
+        String[] array4 = {"Hola", "Adios", null, "Buenos dias", "Buenas tardes", "Buenas noches", null, null};
 
         System.out.println("Array 1");
         mostrarCadenasArray(array);
