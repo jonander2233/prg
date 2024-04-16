eaae7ab4efe591ab7e0aa3281402c926c256e4c4
Jon Ander <jonander2233@gmail.com>
2024-04-16 14:25:45 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema09/ej06/Ej06.java
+++ src/main/java/org/jonander2233/tareas/tema09/ej06/Ej06.java
@@ -1,10 +1,34 @@
 package org.jonander2233.tareas.tema09.ej06;
 
 public class Ej06 {
-    public static void dividirEntreArray (int num, int[] nums){
-        int[] resultado = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-//            nums[i]
+    Ej06(){
+        int[] array = {-2, -1, 0, 1, 2};
+        dividirEntreArray(2, array);
+        dividirEntreArray2(2, array);
+    }
+
+    public static void dividirEntreArray(int num, int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            try {
+                System.out.println(num + " / " + array[i] + " = " + num / array[i]);
+            } catch (ArithmeticException e) {
+                System.out.println("No se puede dividir entre 0");
+            }
+        }
+    }
+
+    public static void dividirEntreArray2(int num, int[] array) {
+
+        for (int i : array){
+            if (i == 0){
+                System.out.println("Hay un 0 en el array");
+                System.out.println("No se puede dividir entre 0");
+                return;
+            }
+        }
+        for (int j : array) {
+            System.out.println(num + " / " + j + " = " + num / j);
         }
     }
 }
---
+++ src/main/java/org/jonander2233/tareas/tema09/ej07/Ej07.java
@@ -0,0 +1,31 @@
+package org.jonander2233.tareas.tema09.ej07;
+public class Ej07 {
+    Ej07(){
+        String[] array = {"Hola", "Adios", null, "Buenos d�as", "Buenas tardes", "Buenas noches"};
+        String[] array2 = {"Hola", "Adios", "Buenos d�as", "Buenas tardes", "Buenas noches"};
+        String[] array3 = {"Hola", "Adios", null, "Buenos d�as", "Buenas tardes", "Buenas noches", null};
+        String[] array4 = {"Hola", "Adios", null, "Buenos d�as", "Buenas tardes", "Buenas noches", null, null};
+
+        System.out.println("Array 1");
+        mostrarCadenasArray(array);
+        System.out.println("--------------------");
+        System.out.println("Array 2");
+        mostrarCadenasArray(array2);
+        System.out.println("--------------------");
+        System.out.println("Array 3");
+        mostrarCadenasArray(array3);
+        System.out.println("--------------------");
+        System.out.println("Array 4");
+        mostrarCadenasArray(array4);
+    }
+
+    public void mostrarCadenasArray(String[] array) {
+        for (String cadena : array) {
+            try {
+                System.out.println(cadena.charAt(0));
+            } catch (NullPointerException e) {
+                System.out.println("nulo");
+            }
+        }
+    }
+}
