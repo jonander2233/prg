e421dc4c405fd1667d880f66b866f05a1d56b0d2
Jon Ander <jonander2233@gmail.com>
2024-04-24 14:35:36 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema10/ej05/Main.java
+++ src/main/java/org/jonander2233/tareas/tema10/ej05/Main.java
@@ -1,10 +1,44 @@
 package org.jonander2233.tareas.tema10.ej05;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main (String[]args){
-        Date fecha = new Date();
-//        Paciente paciente1 = new Paciente("marta",fecha. )
+        EstadisticasPaciente ep = new EstadisticasPaciente();
+        ArrayList<Paciente> arrayP = new ArrayList<>();
+
+        Date fecha1 = new Date(1980,02,12);
+        Date fecha2 = new Date(1990,03,07);
+        Date fecha3 = new Date(1967,03,20);
+        Date fecha4 = new Date(1972,04,20);
+        Date fecha5 = new Date(1960,02,29);
+
+        Paciente paciente1 = new Paciente("marta",fecha1,'F',1.63,57);
+        Paciente paciente2 = new Paciente("carla",fecha2,'F',1.74,60.5);
+        Paciente paciente3 = new Paciente("lucia",fecha3,'F',1.62,50.8);
+        Paciente paciente4 = new Paciente("pedro",fecha4,'M',1.78,72.5);
+        Paciente paciente5 = new Paciente("julio",fecha5,'M',1.8,85.2);
+        arrayP.add(paciente1);
+        arrayP.add(paciente2);
+        arrayP.add(paciente3);
+        arrayP.add(paciente4);
+        arrayP.add(paciente5);
+
+        int[] mm  = ep.majorMenor(arrayP);
+        Paciente pacienteMayor = arrayP.get(mm[1]);
+        Paciente pacienteMenor = arrayP.get(mm[0]);
+
+        System.out.println("PACIENTE MAYOR Edad: " + ep.calcularEdad(pacienteMayor) + " Sexo: " + pacienteMayor.getSexo());
+        System.out.println("PACIENTE MENOR Edad: " + ep.calcularEdad(pacienteMenor) + " Sexo: " + pacienteMenor.getSexo());
+        System.out.println("cantidad de pacientes por sexos:");
+        int[] ps = ep.pacientsPerSexe(arrayP);
+
+        System.out.println("Hombres: " + ps[0] + " Mujeres: "+ ps[1]);
+        for (int i = 0; i < arrayP.size(); i++) {
+            System.out.println("Paciente numero " + (i+1) + ": " + ep.mensajeIMC(arrayP.get(i)));
+        }
+
     }
 }
