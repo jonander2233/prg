015e8867356cf2428fa6b6fbb45b3e2030e45dd1
Jon <jonander2233@gmail.com>
2024-04-30 16:38:03 +0200

    wip

---
+++ src/main/java/org/jonander2233/tareas/tema10/tarea01/RandomSeries.java
@@ -0,0 +1,33 @@
+package org.jonander2233.tareas.tema10.tarea01;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class RandomSeries implements Iterator<Double> {
+    private Random random = new Random();
+    private double min;
+    private double max;
+
+    public RandomSeries() {
+        this(0,1);
+    }
+
+    public RandomSeries(Long seed, int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public RandomSeries(double min, double max){
+//        this(Long.MIN_VALUE, min , max);
+    }
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Double next() {
+        random.nextDouble(min,max);
+        return null;
+    }
+}
