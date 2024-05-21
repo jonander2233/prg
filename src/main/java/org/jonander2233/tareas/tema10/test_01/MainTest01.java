9d36cb1b6ee1bc1cb205aed96a3a90052b251e6e
Jon <jonander2233@gmail.com>
2024-05-21 16:35:05 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema10/test_01/MainTest01.java
+++ src/main/java/org/jonander2233/tareas/tema10/test_01/MainTest01.java
@@ -1,15 +1,15 @@
 package org.jonander2233.tareas.tema10.test_01;
 
 public class MainTest01 {
-    public void main(){
+//    public void main(){
+//
+//    }
 
-    }
-
-    public void metodo01(Interfaz i){
-        i.suma(2,3);
-    }
-
-    private void draw(IRenderer r) {
-        r.render();
-    }
+//    public void metodo01(Interfaz i){
+//        i.suma(2,3);
+//    }
+//
+//    private void draw(IRenderer r) {
+//        r.render();
+//    }
 }
