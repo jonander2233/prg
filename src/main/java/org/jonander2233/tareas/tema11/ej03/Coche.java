1cd6d07a3df47b7f46b4916e2dd1d8bb85305828
Jon <jonander2233@gmail.com>
2024-05-22 15:48:13 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema11/ej03/Coche.java
+++ src/main/java/org/jonander2233/tareas/tema11/ej03/Coche.java
@@ -6,7 +6,7 @@ public class Coche {
     private String matricula;
     private int velocidad;
     private int marcha;
-    private int[] nMarchas = new int[6];
+    private int[] nMarchas = new int[]{60,90,120,150,180};
 
     public Coche(String matricula) {
         this.matricula = matricula;
@@ -17,6 +17,7 @@ public class Coche {
         if(valor < 1){
             return false;
         } else {
+
             velocidad += valor;
         }
         return true;
