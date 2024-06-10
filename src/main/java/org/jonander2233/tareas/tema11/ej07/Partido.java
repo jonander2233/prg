2c161116697ed8749f822f38500a8d2f6904e62e
Jon <jonander2233@gmail.com>
2024-06-10 19:17:37 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema11/ej07/Partido.java
+++ src/main/java/org/jonander2233/tareas/tema11/ej07/Partido.java
@@ -1,5 +1,22 @@
 package org.jonander2233.tareas.tema11.ej07;
-
+enum Afluencia{
+    BAJA_AFLUENCIA(0.75),MEDIA_AFLUENCIA(1),ALTA_AFLUENCIA(1.30);
+    private final double porcentaje;
+    private Afluencia(double porcentaje) {
+            this.porcentaje = porcentaje;
+    }
+    public double getPorcentaje(){
+        return porcentaje;
+    }
+}
 public class Partido {
+    private int id=0;
+    private Afluencia afluencia;
+    private String nombre;
 
+    public Partido(Afluencia afluencia, String nombre) {
+        id++;
+        this.afluencia = afluencia;
+        this.nombre = nombre;
+    }
 }
--- src/main/java/org/jonander2233/tareas/tema11/ej07/Venta.java
+++ src/main/java/org/jonander2233/tareas/tema11/ej07/Venta.java
@@ -1,5 +1,21 @@
 package org.jonander2233.tareas.tema11.ej07;
 
 public class Venta {
+    private int nFYA; //numero de asientos y filas
+    private int nZonas;
+    private int nZonasVIP;
+    Partido partido;
+    Estadio estadio;
 
+    public Venta(String nombrePartido,int nFYA, int nZonas, int nZonasVIP, Estadio estadio,Afluencia afluencia) {
+        this.nFYA = nFYA;
+        this.nZonas = nZonas;
+        this.nZonasVIP = nZonasVIP;
+        this.partido = new Partido(afluencia,nombrePartido);
+        this.estadio = new Estadio(nZonas,nZonasVIP,nFYA);
+
+    }
+    public void venderEntrada(){
+
+    }
 }
