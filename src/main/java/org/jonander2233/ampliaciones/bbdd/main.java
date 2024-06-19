f98ca6ce287c01f3965b9371b12a44fc3555f9d8
Jon <jonander2233@gmail.com>
2024-06-19 17:06:44 +0200

    wip

---
+++ src/main/java/org/jonander2233/ampliaciones/bbdd/main.java
@@ -0,0 +1,24 @@
+package org.jonander2233.ampliaciones.bbdd;
+
+import java.sql.*;
+
+public class main {
+    public static void main(String[] args) {
+        final String HOST = "localhost";
+        final int PORT = 3306;
+        final String DB_NAME = "frases";
+        String url = "jdbc:mysql://"+ HOST +":"+ PORT +"/" + DB_NAME;
+        try (Connection con = DriverManager.getConnection(url)){
+            PreparedStatement ps = con.prepareStatement("SELECT id,frase FROM frase");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String frase = rs.getString("frase");
+                System.out.println(frase);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/jonander2233/ampliaciones/threads/test1/main.java
@@ -0,0 +1,7 @@
+package org.jonander2233.ampliaciones.threads.test1;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
