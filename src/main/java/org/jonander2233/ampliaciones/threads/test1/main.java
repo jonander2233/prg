05ec168a98f2c59872dce925396abe3ad7002fa7
Jon <jonander2233@gmail.com>
2024-06-20 20:43:41 +0200

    wip

--- src/main/java/org/jonander2233/ampliaciones/threads/test1/main.java
+++
@@ -1,7 +0,0 @@
-package org.jonander2233.ampliaciones.threads.test1;
-
-public class main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/org/jonander2233/ampliaciones/web/test1/test1.java
@@ -0,0 +1,7 @@
+package org.jonander2233.ampliaciones.web.test1;
+
+public class test1 {
+    public static void main(String[] args) {
+
+    }
+}
