292ab9bfbab14185884d3fe90882233824c38346
Jon Ander <jonander2233@gmail.com>
2024-07-05 23:00:48 +0200

    wip

--- src/main/java/org/jonander2233/tareas/tema12/MainT12.java
+++ src/main/java/org/jonander2233/tareas/tema12/MainT12.java
@@ -1,9 +1,10 @@
 package org.jonander2233.tareas.tema12;
 
 import org.jonander2233.tareas.tema12.ej04.Ej04;
+import org.jonander2233.tareas.tema12.ej05.GestionArchivos;
 
 public class MainT12 {
     public static void main(String[] args) {
-        Ej04.ejercicio();
+        GestionArchivos.listarDirectorio("D:\\PRG\\prg-jdk20\\src\\main\\java\\org\\jonander2233\\tareas\\tema12\\recursos");
     }
 }
--- src/main/java/org/jonander2233/tareas/tema12/ej05/GestionArchivos.java
+++ src/main/java/org/jonander2233/tareas/tema12/ej05/GestionArchivos.java
@@ -1,18 +1,45 @@
 package org.jonander2233.tareas.tema12.ej05;
 
+import java.io.File;
+import java.io.IOException;
+
 public class GestionArchivos {
-    public void ejercicio(){
 
-    }
-    public boolean crearArchivo(String directorio,String archivo){
-
+    public static boolean crearArchivo(String directorio,String archivo) throws IOException {
+        File dir = new File(directorio);
+        if(dir.exists() && dir.canWrite() && dir.isDirectory()){
+            File nuevoArchivo = new File(dir,archivo);
+            if(!nuevoArchivo.exists()){
+                nuevoArchivo.createNewFile();
+            } else {
+                return false;
+            }
+        }else {
+            return false;
+        }
         return true;
-
     }
-    public void listarDirectorio(String directorio){
 
+    public static void listarDirectorio(String directorio){
+        String tipo;
+        StringBuilder leerYEscribir = new StringBuilder();
+        File dir = new File(directorio);
+        File[] contenido = dir.listFiles();
+        for (int i = 0; i < contenido.length; i++) {
+            if(contenido[i].isDirectory())
+                tipo = "D";
+            else
+                tipo = "F";
+
+            if(contenido[i].canRead())
+                leerYEscribir.append("r");
+            if(contenido[i].canWrite())
+                leerYEscribir.append("w");
+            System.out.println(contenido[i].getName() + " " + tipo + " "+ contenido[i].length() +" bytes " + leerYEscribir.toString());
+            leerYEscribir.setLength(0);
+        }
     }
-    public void verInfo(String directorio,String archivo){
+    public static void verInfo(String directorio,String archivo){
 
     }
 
